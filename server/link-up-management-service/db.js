cf31ce4a0a83a32507522c41f90623572493efd0
Jenise Amoah <amoahjenise@gmail.com>
2024-08-16 00:43:31 -0400

    Password for the Postgres added

--- server/link-up-management-service/db.js
+++ server/link-up-management-service/db.js
@@ -4,11 +4,11 @@ const { Pool } = require("pg");
 // console.log("Loading .env from", require("path").resolve(".env"));
 
 const pool = new Pool({
-  user: process.env.POSTGRES_USER || "postgres",
-  password: process.env.POSTGRES_PASSWORD || "Bl_ablabla))1",
-  host: process.env.POSTGRES_HOST || "localhost",
-  port: process.env.POSTGRES_DBPORT || 5432,
-  database: process.env.POSTGRES_DB || "luul",
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_DBPORT,
+  database: process.env.POSTGRES_DB,
 });
 
 module.exports = {
