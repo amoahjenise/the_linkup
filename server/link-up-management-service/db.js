8560adac90d53b0a8c8609bb7693d91a64a2582b
Jenise Amoah <amoahjenise@gmail.com>
2024-09-02 13:43:43 -0400

    Fix trouble verifying the SSL certificate of the PostgreSQL server.

--- server/link-up-management-service/db.js
+++ server/link-up-management-service/db.js
@@ -4,11 +4,14 @@ const { Pool } = require("pg");
 // console.log("Loading .env from", require("path").resolve(".env"));
 
 const pool = new Pool({
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT,
   user: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
-  host: process.env.POSTGRES_HOST,
-  port: process.env.POSTGRES_DBPORT,
   database: process.env.POSTGRES_DB,
+  ssl: {
+    rejectUnauthorized: false, // Add this line if the certificate is self-signed or not verifiable
+  },
 });
 
 module.exports = {
--- server/notification-service/db.js
+++ server/notification-service/db.js
@@ -2,11 +2,14 @@ require("dotenv").config({ path: "./server/notification-service/.env" });
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT,
   user: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
-  host: process.env.POSTGRES_HOST,
-  port: process.env.POSTGRES_DBPORT,
   database: process.env.POSTGRES_DB,
+  ssl: {
+    rejectUnauthorized: false, // Add this line if the certificate is self-signed or not verifiable
+  },
 });
 
 module.exports = {
