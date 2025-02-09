8ea9f3142ae7cccb66d8d1a93e983f271a53b05c
Jenise Amoah <amoahjenise@gmail.com>
2025-02-08 23:41:10 -0500

    Fixed error page layout

--- client/src/components/ErrorPage.js
+++ client/src/components/ErrorPage.js
@@ -1,24 +1,28 @@
 import React from "react";
 import { styled } from "@mui/material/styles";
 import { Button } from "@mui/material";
-import Logo from "../assets/logo.png"; // Import the logo image
-import { useColorMode } from "@chakra-ui/react";
 
 // Define styled components
-const ErrorContainer = styled("div")({
+const ErrorContainer = styled("div")(({ theme, colorMode }) => ({
+  position: "relative",
   display: "flex",
   flexDirection: "column",
+  borderRadius: "8px", // Subtle rounded corners for the whole container
+  boxShadow: "0 4px 12px rgba(0, 0, 0, 0.1)", // Soft shadow
+  maxWidth: "calc(100vw - 80px)", // Adjust width to account for the left menu (assuming it's 80px wide)
+  minHeight: "100vh",
   alignItems: "center", // Center horizontally
   justifyContent: "center", // Center vertically
-  minHeight: "100vh", // Ensure the container takes full height of the viewport
   textAlign: "center", // Center text horizontally
-});
-
-const LogoImage = styled("img")(({ theme }) => ({
-  width: 80,
-  marginBottom: 20,
-  filter:
-    theme.palette.mode === "dark" ? "invert(0.879) grayscale(70%)" : "none",
+  flex: 2,
+  overflowY: "auto", // Enables vertical scrolling if content overflows
+  borderRightWidth: "1px",
+  borderRightColor: colorMode === "dark" ? "#2D3748" : "#D3D3D3", // Border color based on theme
+  marginLeft: "80px", // Adjust for the left menu width
+  [theme.breakpoints.down("sm")]: {
+    flex: 1, // Adjust flex for smaller screens
+    marginLeft: 0, // Remove left margin on smaller screens
+  },
 }));
 
 const ErrorMessage = styled("h1")(({ theme }) => ({
@@ -39,18 +43,12 @@ const RetryButton = styled(Button)(({ theme }) => ({
 }));
 
 const ErrorPage = ({ onRetry }) => {
-  const { colorMode } = useColorMode();
-
   return (
     <ErrorContainer>
-      <LogoImage src={Logo} alt="The Linkup Logo" />
       <ErrorMessage>Oops! Something went wrong.</ErrorMessage>
       <ErrorDescription>
         We're sorry, but we couldn't fetch the data from our servers.
       </ErrorDescription>
-      <RetryButton variant="contained" onClick={onRetry}>
-        Retry
-      </RetryButton>
       <ErrorDescription>
         If the problem persists, please contact support.
       </ErrorDescription>
