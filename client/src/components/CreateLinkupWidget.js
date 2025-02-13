97843fcf44f117580cbac6962d930e43c8cb2664
Jenise Amoah <amoahjenise@gmail.com>
2025-02-13 13:06:20 -0500

    Reverted to old texts Man/Woman

--- client/src/components/CreateLinkupWidget.js
+++ client/src/components/CreateLinkupWidget.js
@@ -234,8 +234,8 @@ const CreateLinkupWidget = ({
 
   // Gender options for MultiSelect
   const genderOptions = [
-    { key: "man", value: "Men" },
-    { key: "woman", value: "Women" },
+    { key: "man", value: "Man" },
+    { key: "woman", value: "Woman" },
     ...customGenderOptions.map((gender) => ({
       key: gender.toLowerCase(),
       value: gender,
