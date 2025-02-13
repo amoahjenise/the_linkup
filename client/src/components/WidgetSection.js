fcd3806c67d71088248cf5a87a026034c55f56a9
Jenise Amoah <amoahjenise@gmail.com>
2025-02-13 13:22:16 -0500

    fixed issue with search

--- client/src/components/WidgetSection.js
+++ client/src/components/WidgetSection.js
@@ -1,11 +1,6 @@
 import React from "react";
-// import { useDispatch } from "react-redux";
-// import SearchInput from "./SearchInputWidget";
 import CreateLinkupForm from "./CreateLinkupWidget";
 import { styled } from "@mui/material/styles";
-// import { searchLinkups } from "../api/linkUpAPI";
-// import { fetchLinkupsSuccess } from "../redux/actions/linkupActions";
-// import debounce from "lodash/debounce"; // Import debounce function from lodash
 import CategoryWidget from "./CategoryWidget";
 
 const WidgetSectionContainer = styled("div")(({ theme }) => ({
@@ -24,32 +19,7 @@ const WidgetSection = ({
   setIsWidgetVisible,
   setShouldFetchLinkups,
   scrollToTopCallback,
-  // userId,
-  // gender,
 }) => {
-  // const dispatch = useDispatch();
-  // const [loading, setLoading] = useState(false);
-
-  // // Persist debounced function using useRef to avoid recreation
-  // const debounceSearchRef = useRef(
-  //   debounce(async (value) => {
-  //     setLoading(true);
-  //     try {
-  //       const response = await searchLinkups(value, userId, gender);
-  //       dispatch(fetchLinkupsSuccess(response.linkupList));
-  //     } catch (error) {
-  //       console.error("Error fetching linkups:", error);
-  //     } finally {
-  //       setLoading(false);
-  //     }
-  //   }, 300)
-  // );
-
-  // // Function to handle input change and trigger search
-  // const handleInputChange = (event) => {
-  //   debounceSearchRef.current(event.target.value);
-  // };
-
   const handleFilterChange = (selectedCategories) => {
     console.log("Selected Categories:", selectedCategories);
     // Apply filter logic to the feed based on selectedCategories
@@ -57,11 +27,6 @@ const WidgetSection = ({
 
   return (
     <WidgetSectionContainer>
-      {/* Search Input Component */}
-      {/* <Widget>
-        <SearchInput handleInputChange={handleInputChange} loading={loading} />
-      </Widget> */}
-
       {/* Create Linkup Component */}
       <Widget>
         <CreateLinkupForm
