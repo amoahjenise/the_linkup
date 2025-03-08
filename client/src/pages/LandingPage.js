4d7890786981ac18507b19671b9281f1c6cdc353
Jenise Amoah <amoahjenise@gmail.com>
2025-03-08 16:38:58 -0500

    New landing page design

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -1,97 +1,11 @@
-import React, { useEffect } from "react";
-import { useSelector } from "react-redux";
-import { styled } from "@mui/material/styles";
-import { Typography } from "@mui/material";
-import { useNavigate } from "react-router-dom";
-import { useColorMode } from "@chakra-ui/react";
-import logoSrc from "../assets/logo.png";
-import AppDarkMode from "../assets/LandingPagePreview1.png";
-import AppLightMode from "../assets/LandingPagePreview1.png";
-import Wallpaper from "../assets/Image1.jpg";
-import { SignInButton, SignUpButton } from "@clerk/clerk-react";
+import React, { memo } from "react";
+import { Box, Grid, Typography, Card, CardContent } from "@mui/material";
 import LogoHeader from "../components/LogoHeader";
-
-// Styled Components
-const PageContainer = styled("div")({
-  display: "flex",
-  flexDirection: "column",
-  minHeight: "100vh",
-  background: `url(${Wallpaper}) no-repeat center center fixed`,
-  backgroundSize: "cover",
-  padding: "20px",
-});
-
-const Logo = styled("img")({
-  width: "30px",
-  height: "30px",
-  marginRight: "10px",
-  filter: "invert(1)", // White logo for dark backgrounds
-});
-
-const ContentContainer = styled("main")(({ theme }) => ({
-  display: "flex",
-  flex: 1,
-  padding: theme.spacing(3),
-  justifyContent: "center",
-  alignItems: "center",
-  flexDirection: "column",
-  [theme.breakpoints.up("md")]: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-  },
-  [theme.breakpoints.down("sm")]: {
-    padding: theme.spacing(2),
-    flexDirection: "column", // Stack elements in a column
-    justifyContent: "center",
-    alignItems: "center",
-  },
-}));
-
-const LeftColumn = styled("div")(({ theme }) => ({
-  flex: 1,
-  textAlign: "center",
-  padding: theme.spacing(3),
-  backgroundColor: "rgba(255, 255, 255, 0.1)",
-  backdropFilter: "blur(10px)",
-  borderRadius: "8px",
-  boxShadow: "0 4px 10px rgba(0, 0, 0, 0.1)",
-  [theme.breakpoints.up("md")]: {
-    textAlign: "left",
-  },
-  [theme.breakpoints.down("sm")]: {
-    padding: theme.spacing(2),
-    backgroundColor: "transparent",
-    backdropFilter: "none",
-    boxShadow: "none",
-    marginBottom: theme.spacing(3),
-  },
-}));
-
-const RightColumn = styled("div")(({ theme }) => ({
-  flex: 1,
-  display: "none",
-  justifyContent: "center",
-  alignItems: "center",
-  padding: theme.spacing(1),
-  [theme.breakpoints.up("md")]: {
-    display: "flex", // Show on larger screens
-  },
-  [theme.breakpoints.down("sm")]: {
-    display: "none", // Hide on smaller screens
-  },
-}));
-
-const ResponsiveImage = styled("img")(({ theme }) => ({
-  maxHeight: "380px", // Limit the maximum height
-  width: "auto", // Maintain the aspect ratio
-  maxWidth: "100%", // Prevent overflow while allowing it to be responsive
-  borderRadius: "8px", // Softer corners
-  boxShadow: "0 1px 3px rgba(0, 0, 0, 0.2)", // Subtle shadow for a clean look
-  transition: "transform 0.3s ease", // Smooth hover effect
-  "&:hover": {
-    transform: "scale(1.02)", // Slight zoom effect on hover for interaction
-  },
-}));
+import WidgetTemplate from "../components/WidgetTemplate";
+import { styled } from "@mui/material/styles";
+import Wallpaper from "../assets/Image5.jpg";
+import Wallpaper2 from "../assets/Image2.jpg";
+import { useTheme } from "@mui/material/styles";
 
 const Footer = styled("footer")(({ theme }) => ({
   display: "flex",
@@ -108,164 +22,164 @@ const Footer = styled("footer")(({ theme }) => ({
   },
 }));
 
-const Title = styled("h1")(({ theme }) => ({
-  fontSize: "2rem",
-  fontWeight: "bold",
-  color: "white",
-  [theme.breakpoints.up("sm")]: {
-    fontSize: "2.5rem",
-  },
-  [theme.breakpoints.up("md")]: {
-    fontSize: "3rem",
-  },
-  textShadow: "2px 2px 5px rgba(0, 0, 0, 0.5)",
-}));
+const Logo = styled("img")({
+  width: "30px",
+  height: "30px",
+  marginRight: "10px",
+  filter: "invert(1)", // White logo for dark backgrounds
+});
 
-const Subtitle = styled(Typography)(({ theme }) => ({
-  color: "white",
-  marginTop: theme.spacing(3),
-  fontSize: "1.3rem",
-  fontWeight: "600px",
-  [theme.breakpoints.up("sm")]: {
-    fontSize: "1.3rem",
-  },
-  [theme.breakpoints.up("md")]: {
-    fontSize: "1.6rem",
-  },
-  textShadow: "2px 2px 10px rgba(0, 0, 0, 0.9)", // Stronger text shadow for better contrast
-}));
+const SignUpWidget = memo(() => (
+  <Card
+    sx={{
+      cursor: "default",
+      position: "relative",
+      backgroundImage: `url(${Wallpaper2})`, // Set image as background
+      backgroundSize: "cover",
+      backgroundPosition: "center",
+      backdropFilter: "blur(10px)",
+      borderRadius: "20px",
+      padding: "20px",
+      boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.2)",
+      transition: "transform 0.3s ease",
+      "&:hover": {
+        transform: "scale(1.05)",
+      },
+      maxWidth: "225px", // Max width for better layout on medium screens
+      height: "225px",
+      width: "225px", // Max width for better layout on medium screens
+      marginBottom: "20px", // Ensure there's spacing between widgets
+    }}
+  >
+    <CardContent sx={{ textAlign: "center", color: "white" }}>
+      <Typography variant="h5" sx={{ fontWeight: "bold", mb: 2 }}>
+        Join The Linkup Platform
+      </Typography>
+    </CardContent>
+  </Card>
+));
 
-const StyledButton = styled(SignUpButton)(({ theme }) => ({
-  textTransform: "none",
-  padding: theme.spacing(1.5, 4),
-  fontSize: "1rem",
-  fontWeight: "bold",
-  border: "2px solid rgb(255, 255, 255)", // Border to match button text
-  background: "linear-gradient(45deg,rgb(51, 51, 51, 0.7), #5C6BC0)", // Sleek charcoal and slate blue gradient
-  color: "#FFFFFF",
-  borderRadius: "30px",
-  boxShadow: "0 4px 6px rgba(0, 0, 0, 0.2)",
-  transition: "transform 0.2s ease, box-shadow 0.2s ease, background 0.3s ease", // Shorter transition for a subtler effect
-  "&:hover": {
-    transform: "translateY(-2px)",
-    boxShadow: "0 4px 6px rgba(0, 0, 0, 0.2)", // Subtle shadow change
-  },
-  margin: theme.spacing(1, 0),
-  [theme.breakpoints.down("sm")]: {
-    width: "100%", // Full width button for smaller screens
-  },
-}));
-
-const OutlinedButton = styled(SignInButton)(({ theme }) => ({
-  textTransform: "none",
-  padding: theme.spacing(1.5, 4),
-  fontSize: "1rem",
-  fontWeight: "bold",
-  border: "2px solid rgb(255, 255, 255)", // Border to match button text
-  borderRadius: "30px",
-  color: "#FFFFFF",
-  background:
-    "linear-gradient(45deg, rgba(255, 255, 255, 0.5), rgba(63, 81, 181, 0.75))", // Subtle gradient with blue tones
-  transition:
-    "background-color 0.3s ease, color 0.3s ease, transform 0.3s ease",
-  "&:hover": {
-    backgroundColor: "linear-gradient(45deg, #4CAF50, #2196F3)", // Match the primary button gradient on hover
-    color: "#FFFFFF", // Change text to white on hover for contrast
-    transform: "translateY(-2px)",
-  },
-  margin: theme.spacing(1, 0),
-  [theme.breakpoints.down("sm")]: {
-    width: "100%", // Full width button for smaller screens
-  },
-}));
-
-const ButtonContainer = styled("div")(({ theme }) => ({
-  display: "flex",
-  flexDirection: "column",
-  gap: theme.spacing(2), // Add space between buttons
-  marginTop: theme.spacing(3),
-  [theme.breakpoints.down("sm")]: {
-    width: "100%", // Full width container for smaller screens
-  },
-}));
+const TermsAndServiceWidget = memo(() => (
+  <Card
+    sx={{
+      cursor: "default",
+      position: "relative",
+      background: `linear-gradient(45deg, rgba(211, 254, 255, 0.85), rgba(30, 73, 79, 0.75))`,
+      backdropFilter: "blur(10px)",
+      borderRadius: "20px",
+      padding: "20px",
+      boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.2)",
+      transition: "transform 0.3s ease",
+      "&:hover": {
+        transform: "scale(1.05)",
+      },
+      width: "225px", // Max width for better layout on medium screens
+      maxWidth: "225px", // Max width for better layout on medium screens
+      height: "225px",
+    }}
+  >
+    <CardContent sx={{ textAlign: "center" }}>
+      <Typography variant="h6" color="white" sx={{ fontWeight: "bold" }}>
+        By signing up, you agree to the
+      </Typography>
+      <Typography variant="body1" sx={{ mt: 2, color: "white" }}>
+        <a href="/terms-of-service" style={{ color: "white" }}>
+          Terms of Service
+        </a>{" "}
+        ,{" "}
+        <a href="/privacy-policy" style={{ color: "white" }}>
+          Privacy Policy
+        </a>{" "}
+        and{" "}
+      </Typography>
+      <Typography variant="body2" sx={{ color: "gray" }}>
+        <a href="/cookie-use" style={{ color: "white" }}>
+          Cookie Use
+        </a>
+        .
+      </Typography>
+    </CardContent>
+  </Card>
+));
 
 const LandingPage = () => {
-  const navigate = useNavigate();
-  const { isAuthenticated } = useSelector((state) => state.auth);
-  const { colorMode } = useColorMode();
-
-  useEffect(() => {
-    if (isAuthenticated) {
-      navigate("/home");
-    }
-  }, [isAuthenticated, navigate]);
+  const theme = useTheme(); // Make sure theme is initialized using useTheme
 
   return (
-    <PageContainer>
-      <ContentContainer>
-        <LeftColumn>
-          {/* <Title>The Linkup</Title> */}
-          <LogoHeader />
-          <Subtitle>
-            Connect with new people and organize meetups around your interests.
-          </Subtitle>
-          <Typography
-            variant="subtitle2"
-            component="small"
-            sx={{
-              marginTop: 2,
-              color: "#FFFFFF",
-              textShadow: "2px 2px 5px rgba(0, 0, 0, 0.5)",
-              fontSize: "16px",
-            }}
-          >
-            By signing up, you agree to the{" "}
-            <a href="/terms-of-service" style={{ color: "white" }}>
-              Terms of Service
-            </a>{" "}
-            and{" "}
-            <a href="/privacy-policy" style={{ color: "white" }}>
-              Privacy Policy
-            </a>
-            , including{" "}
-            <a href="/cookie-use" style={{ color: "white" }}>
-              Cookie Use
-            </a>
-            .
-          </Typography>
-          <ButtonContainer>
-            <StyledButton
-              forceRedirectUrl="/registration" // Redirect to /registration after signing up
-              fallbackRedirectUrl="/registration"
-              mode="modal"
-            >
-              Sign Up
-            </StyledButton>
-            <OutlinedButton
-              forceRedirectUrl="/home" // Redirect to /home after signing in
-              fallbackRedirectUrl="/home"
-              mode="modal"
-            >
-              Sign In
-            </OutlinedButton>
-          </ButtonContainer>
-        </LeftColumn>
-
-        <RightColumn>
-          <ResponsiveImage
-            src={colorMode === "dark" ? AppDarkMode : AppLightMode}
-            alt="App Mode"
+    <Box
+      sx={{
+        padding: "40px",
+        background: "linear-gradient(135deg, rgb(0, 0, 0), rgb(15, 0, 38))",
+        minHeight: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        color: "white",
+      }}
+    >
+      <LogoHeader />
+      <Grid
+        container
+        spacing={2}
+        justifyContent="center"
+        sx={{
+          maxWidth: "1200px",
+          paddingTop: "40px",
+          paddingBottom: "40px",
+          [theme.breakpoints.down("md")]: {
+            flexDirection: "column", // Stack columns on medium screens
+            alignItems: "center", // Center the content for small screens
+          },
+        }}
+      >
+        {/* WidgetTemplate - Left Side */}
+        <Grid
+          item
+          xs={12}
+          sm={6}
+          md={3}
+          sx={{
+            cursor: "pointer",
+            transition: "transform 0.3s ease",
+            "&:hover": {
+              transform: "scale(1.02)",
+            },
+            marginBottom: "20px", // Ensure there's spacing on smaller screens
+          }}
+        >
+          <WidgetTemplate
+            image={Wallpaper}
+            title="Connect With New People and Organize Meetups"
+            subtitle="Around your interests"
           />
-        </RightColumn>
-      </ContentContainer>
+        </Grid>
+
+        {/* Right Side - Stacked Widgets */}
+        <Grid
+          item
+          xs={12}
+          sm={6}
+          md={3} // Updated to 3 for better balance on larger screens
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            gap: 2, // Increase gap between widgets
+            alignItems: "center", // Center widgets on small screens
+            justifyContent: "flex-start", // Ensure widgets are aligned to the top
+          }}
+        >
+          <TermsAndServiceWidget />
+          <SignUpWidget />
+        </Grid>
+      </Grid>
 
       <Footer>
-        <Logo src={logoSrc} alt="The Linkup Logo" />
         <Typography variant="body2">
           &copy; {new Date().getFullYear()} The Linkup. All rights reserved.
         </Typography>
       </Footer>
-    </PageContainer>
+    </Box>
   );
 };
 
