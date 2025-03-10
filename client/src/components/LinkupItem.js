b3cc2e06347c34d6c56ce9cc5437a0de6412fcd0
Jenise Amoah <amoahjenise@gmail.com>
2025-03-10 16:58:57 -0400

    Removed blurr on linkup item

--- client/src/components/LinkupItem.js
+++ client/src/components/LinkupItem.js
@@ -40,7 +40,7 @@ const CardContainer = styled("div")(({ theme, isHovered, colorMode }) => ({
   width: "100%",
   minHeight: "175px",
   backgroundColor: colorMode === "dark" ? "rgb(10, 70, 87, 0.2)" : "#FFFFFF",
-  backdropFilter: "blur(12px)",
+
   boxShadow:
     colorMode === "light"
       ? isHovered
