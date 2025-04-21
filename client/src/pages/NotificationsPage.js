637b1e56cc21e3f498db482e7be5bb6a7e8b0e89
Jenise Amoah <amoahjenise@gmail.com>
2025-04-21 10:16:27 -0400

    Responsiveness improvements

--- client/src/pages/NotificationsPage.js
+++ client/src/pages/NotificationsPage.js
@@ -1,29 +1,31 @@
+// NotificationsPage.js
 import React from "react";
 import { styled } from "@mui/material/styles";
 import Notifications from "../components/Notifications";
 import { useColorMode } from "@chakra-ui/react";
 
-// Define styled components
 const NotificationsPageWrapper = styled("div")(({ theme }) => ({
   display: "flex",
   flexDirection: "column",
   width: "100%",
+  maxWidth: "1536px", // Added max-width for extra large screens
+  margin: "0 auto", // Center the content
+  height: "100vh",
   [theme.breakpoints.down("md")]: {
-    width: "100%", // Set to 100% in mobile mode
-  },
-  "@media (max-width: 900px)": {
-    marginBottom: "65px", // Add padding for footer
+    width: "100%",
   },
 }));
 
-const NotificationSection = styled("div")({
+const NotificationSection = styled("div")(({ theme }) => ({
   display: "flex",
   flexDirection: "column",
   overflowY: "auto",
-});
+  height: "100%",
+  padding: 0,
+}));
 
 const NotificationsPage = ({ isMobile }) => {
-  const { colorMode } = useColorMode(); // Add this line if using Chakra UI
+  const { colorMode } = useColorMode();
 
   return (
     <NotificationsPageWrapper>
